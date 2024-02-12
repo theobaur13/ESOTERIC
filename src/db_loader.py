6b94cbe968a891d3346a234590c4783190d6dde0
Theo Baur <baurt@cardiff.ac.uk>
2024-02-12 17:23:04 +0000

    added TF-IDF and term database generation

--- src/db_loader.py
+++ src/db_loader.py
@@ -1,35 +1,129 @@
 import os
 import sqlite3
 import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tqdm import tqdm
+import time
+import argparse
 
-def main():
+def main(batch_limit=None):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     dataset_path = os.path.join(current_dir, '..', 'data', 'wiki-pages')
     database_path = os.path.join(current_dir, '..', 'data')
 
     conn = sqlite3.connect(os.path.join(database_path, 'wiki-pages.db'))
+    cursor = conn.cursor()
 
-    conn.execute('''
+    clear = input("Do you want to clear the database? (y/n): ")
+    if clear.lower() == 'y':
+        cursor.execute("DROP TABLE IF EXISTS documents")
+        cursor.execute("DROP TABLE IF EXISTS terms")
+        cursor.execute("DROP TABLE IF EXISTS tf_idf")
+        conn.commit()
+
+    start_time = time.time()
+
+    cursor.execute('''
         CREATE TABLE IF NOT EXISTS documents(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         doc_id TEXT NOT NULL,
         text TEXT NOT NULL);
         ''')
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS terms(
+        term_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        term TEXT NOT NULL UNIQUE);
+        ''')
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tf_idf(
+        term_id INTEGER NOT NULL,
+        doc_id INTEGER NOT NULL,
+        tf_idf_score REAL NOT NULL,
+        FOREIGN KEY(term_id) REFERENCES terms(term_id),
+        FOREIGN KEY(doc_id) REFERENCES documents(id),
+        PRIMARY KEY(term_id, doc_id));
+        ''')
+    
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_documents_doc_id ON documents(doc_id);")
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_tf_idf_term_id ON tf_idf(term_id);")
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_tf_idf_doc_id ON tf_idf(doc_id);")
+    cursor.execute("CREATE INDEX IF NOT EXISTS idx_tf_idf_term_doc ON tf_idf(term_id, doc_id);")
+        
+    doc_ids = []
+    documents = []
+
+    TF_IDF_vectoriser = TfidfVectorizer()
 
     # import jsonl file by file into the db
+    print("Loading " + str(batch_limit) + " documents into database")
     file_list = os.listdir(dataset_path)
-    for file in file_list:
+
+    if batch_limit:
+        file_list = file_list[:batch_limit]
+
+    for file in tqdm(file_list):
         file_path = os.path.join(dataset_path, file)
-        print ("Loading file: " + file)
         
         if file_path.endswith('.jsonl'):
             with open(file_path) as f:
                 for line in f:
                     data = json.loads(line)
-                    conn.execute("INSERT INTO documents (doc_id, text) VALUES (?, ?)", (data['id'], data['text']))
+                    documents.append(data['text'])
+                    doc_ids.append(data['id'])
+                    cursor.execute("INSERT INTO documents (doc_id, text) VALUES (?, ?)", (data['id'], data['text']))
         else:
             raise ValueError('Unsupported file format')
     conn.commit()
 
+    print("Initialising TF IDF matrix")
+    TF_IDF_matrix = TF_IDF_vectoriser.fit_transform(documents)
+
+    print("Applying feature names to TF IDF matrix")
+    feature_names = TF_IDF_vectoriser.get_feature_names_out()
+
+    print ("Loading terms into database")
+    for term in tqdm(feature_names):
+        cursor.execute("INSERT OR IGNORE INTO terms (term) VALUES (?)", (term,))
+    conn.commit()
+
+    print ("Creating term mappings")
+    cursor.execute("SELECT term_id, term FROM terms")
+    term_mapping = {term: term_id for term_id, term in tqdm(cursor.fetchall())}
+
+    print("Calculating TF-IDF values")
+    bulk_data = []
+    for doc_index, doc_id in enumerate(tqdm(doc_ids)):
+        for term_index in TF_IDF_matrix[doc_index].nonzero()[1]:
+            term_id = term_mapping[feature_names[term_index]]
+            tf_idf_score = TF_IDF_matrix[doc_index, term_index]
+            bulk_data.append((term_id, doc_id, tf_idf_score))
+    
+    print("Loading TF-IDF values into database")
+    conn.execute("PRAGMA synchronous = OFF")
+    conn.execute("PRAGMA journal_mode = OFF")
+    conn.execute("PRAGMA cache_size = 1000000")
+    conn.execute("PRAGMA temp_store = MEMORY")
+    conn.execute("PRAGMA locking_mode = EXCLUSIVE")
+
+    chunk_size = 100000
+    total_chunks = len(bulk_data) // chunk_size + (1 if len(bulk_data) % chunk_size > 0 else 0)
+
+    for i in tqdm(range(total_chunks)):
+        start_index = i * chunk_size
+        end_index = start_index + chunk_size
+        chunk = bulk_data[start_index:end_index]
+        cursor.executemany("INSERT INTO tf_idf (term_id, doc_id, tf_idf_score) VALUES (?, (SELECT id FROM documents WHERE doc_id = ?), ?)", chunk)
+
+    conn.commit()
+
+    end_time = time.time()
+    print("Database loaded successfully in " + str(end_time - start_time) + " seconds")
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Load wiki-pages into database.')
+    parser.add_argument('--batch_limit', type=int, help='Limit the number of files processed. Leave blank to process all files.', nargs='?', const=None)
+    args = parser.parse_args()
+    
+    main(batch_limit=args.batch_limit)
