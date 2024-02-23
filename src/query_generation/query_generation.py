a4b8ad3d1d0ace89a83ae6e3c9a9c42a19761547
Theo Baur <baurt@cardiff.ac.uk>
2024-02-23 15:48:28 +0000

    restructured query_generation folder

--- src/query_generation/query_generation.py
+++ src/query_generation/query_generation.py
@@ -1,6 +1,7 @@
 import spacy
 from models import Query, QueryWrapper
 from transformers import pipeline
+from query_generation.tools.varifocal import extract_focals, generate_questions, reranking
 
 class QueryGenerator:
     def __init__(self, claim=""):
@@ -12,8 +13,8 @@ class QueryGenerator:
         self.text = claim
 
     def generate_queries(self):
-        focal_points = self.extract_focals()
-        questions = self.generate_questions(focal_points)
+        focal_points = extract_focals(self.nlp, self.text)
+        questions = generate_questions(self.pipe, self.text, focal_points)
 
         base_claim = Query(self.text)
         query_wrapper = QueryWrapper(base_claim)
@@ -23,50 +24,3 @@ class QueryGenerator:
             query_wrapper.add_query(query)
 
         return query_wrapper
-
-    def extract_focals(self):
-        print("Extracting focal points from claim")
-        doc = self.nlp(self.text)
-        sentences = doc.sents
-
-        tag_set = {
-            "clause_relations": ["csubj", "xcomp", "ccomp", "advcl", "acl"],
-            "sentence_core": ["nsubj", "obj", "iobj"],
-            "full_syntax": ["csubj", "xcomp", "ccomp", "advcl", "acl", "nsubj", "obj", "iobj"],
-            "noun_modifiers": ["nmod", "amod", "obj", "num"],
-            "phrase_details": ["nsubj", "obj", "iobj", "nmod", "amod", "obj", "num"],
-            "syntax_all": ["csubj", "xcomp", "ccomp", "advcl", "acl", "nsubj", "obj", "iobj", "nmod", "amod", "obj", "num"],
-            "sub_verb_obj": ["agent", "ccomp", "csubj", "dobj", "nsubj", "nsubjpass", "pcomp", "pobj", "root"]
-        }
-
-        active_tag_set = tag_set["sub_verb_obj"]
-        subtrees = []
-
-        for sentence in sentences:
-            for token in sentence:
-                if token.dep_ in active_tag_set:
-                    subtree = [t.text for t in token.subtree]
-                    subtrees.append([" ".join(subtree), token.dep_])
-
-        print("Focal points:", subtrees, "extracted.")
-        return subtrees
-    
-    def generate_questions(self, focal_points):
-        print("Generating questions from focal points")
-        questions = []
-
-        for focal_point in focal_points:
-            focal_text = focal_point[0]
-            pipe_string = "answer: " + str(focal_text) + " context: " + str(self.text)
-
-            output = self.pipe(pipe_string)
-            
-            question = output[0]['generated_text'].split("question: ")[1]
-            questions.append(question)
-        
-        print("Questions generated:", questions)
-        return questions
-    
-    def reranking(self, questions):
-        print("Reranking questions")
-        return questions
